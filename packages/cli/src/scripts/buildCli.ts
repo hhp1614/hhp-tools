46ce16f6c57fd37e7fad2f2e229e8d76ba59cfd2
hhp1614 <hhp1614@qq.com>
2021-12-15 12:23:33 +0800

    style(cli): format code

--- packages/cli/src/scripts/buildCli.ts
+++ packages/cli/src/scripts/buildCli.ts
@@ -8,16 +8,16 @@ const buildCli = new Command('build-cli');
 buildCli.command('build').description('打包 cli');
 buildCli.option('-w, --watch', '监视模式');
 buildCli.action(async (option: { watch?: boolean }) => {
-  const rollupOption: RollupOptions = {
-    input: './src/index.ts',
-    output: {
-      file: './dist/bin.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    plugins: [addShebang({ include: ['./dist/bin.js'] })],
-  };
-  await bundle(rollupOption, !!option.watch);
+    const rollupOption: RollupOptions = {
+        input: './src/index.ts',
+        output: {
+            file: './dist/bin.js',
+            format: 'cjs',
+            sourcemap: true,
+        },
+        plugins: [addShebang({ include: ['./dist/bin.js'] })],
+    };
+    await bundle(rollupOption, !!option.watch);
 });
 
 buildCli.parse();
--- packages/cli/src/utils/bundle.ts
+++ packages/cli/src/utils/bundle.ts
@@ -13,59 +13,59 @@ import * as fs from 'fs-extra';
  * @param isWatch 是否监听
  */
 export async function bundle(rollupOptions: RollupOptions, isWatch: boolean) {
-  /** 默认插件 */
-  const defaultPlugins = [typescript(), externals(), json(), terser()];
-  // 解决运行命令时当前目录没有 package.json 文件的报错
-  if (fs.pathExistsSync('./package.json')) {
-    defaultPlugins.push(autoExternal());
-  }
+    /** 默认插件 */
+    const defaultPlugins = [typescript(), externals(), json(), terser()];
+    // 解决运行命令时当前目录没有 package.json 文件的报错
+    if (fs.pathExistsSync('./package.json')) {
+        defaultPlugins.push(autoExternal());
+    }
 
-  rollupOptions.plugins = [...defaultPlugins, ...(rollupOptions.plugins ?? [])];
-  // 如果 output 选项不是数组，将其转化为数组
-  if (!Array.isArray(rollupOptions.output)) {
-    rollupOptions.output = [rollupOptions.output!];
-  }
+    rollupOptions.plugins = [...defaultPlugins, ...(rollupOptions.plugins ?? [])];
+    // 如果 output 选项不是数组，将其转化为数组
+    if (!Array.isArray(rollupOptions.output)) {
+        rollupOptions.output = [rollupOptions.output!];
+    }
 
-  /** rollup 配置列表 */
-  const rollupOptionsList: RollupOptions[] = rollupOptions.output.map(output => {
-    return {
-      input: rollupOptions.input,
-      output,
-      plugins: rollupOptions.plugins,
-    } as RollupOptions;
-  });
-
-  // 是否监听
-  if (isWatch) {
-    const watcherOptionsList = rollupOptionsList.map(option => {
-      // 监听模式排除代码压缩插件
-      return {
-        ...option,
-        plugins: option.plugins?.filter(plugin => (plugin as Plugin)?.name !== 'terser'),
-      };
+    /** rollup 配置列表 */
+    const rollupOptionsList: RollupOptions[] = rollupOptions.output.map(output => {
+        return {
+            input: rollupOptions.input,
+            output,
+            plugins: rollupOptions.plugins,
+        } as RollupOptions;
     });
-    const watcher = watch(watcherOptionsList);
-    watcher.on('event', e => {
-      switch (e.code) {
-        case 'START':
-          console.log(chalk.blue('正在编译'));
-          break;
-        case 'END':
-          console.log(chalk.yellow('编译成功'), chalk.gray('监听文件修改中'));
-          break;
-        case 'ERROR':
-          console.log(chalk.red('编译错误'));
-          break;
-      }
-    });
-    return;
-  }
 
-  /** rollup 打包列表 */
-  const promiseList = rollupOptionsList.map(async option => {
-    const bundle = await rollup(option);
-    await bundle.write(option.output as OutputOptions);
-    await bundle.close();
-  });
-  await Promise.all(promiseList);
+    // 是否监听
+    if (isWatch) {
+        const watcherOptionsList = rollupOptionsList.map(option => {
+            // 监听模式排除代码压缩插件
+            return {
+                ...option,
+                plugins: option.plugins?.filter(plugin => (plugin as Plugin)?.name !== 'terser'),
+            };
+        });
+        const watcher = watch(watcherOptionsList);
+        watcher.on('event', e => {
+            switch (e.code) {
+                case 'START':
+                    console.log(chalk.blue('正在编译'));
+                    break;
+                case 'END':
+                    console.log(chalk.yellow('编译成功'), chalk.gray('监听文件修改中'));
+                    break;
+                case 'ERROR':
+                    console.log(chalk.red('编译错误'));
+                    break;
+            }
+        });
+        return;
+    }
+
+    /** rollup 打包列表 */
+    const promiseList = rollupOptionsList.map(async option => {
+        const bundle = await rollup(option);
+        await bundle.write(option.output as OutputOptions);
+        await bundle.close();
+    });
+    await Promise.all(promiseList);
 }
